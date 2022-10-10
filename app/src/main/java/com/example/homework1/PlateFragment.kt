63519bdb642a98225e8d972fe642c043089fdeb9
MoonLoone <mmaks02@yandex.ru>
2022-10-10 19:05:29 +0300

    Доделал весь функционал

--- app/src/main/java/com/example/homework1/PlateFragment.kt
+++ app/src/main/java/com/example/homework1/PlateFragment.kt
@@ -1,6 +1,5 @@
 package com.example.homework1
 
-import com.example.homework1.adapters.RVPlateAdapter
 import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,12 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
+import com.example.homework1.adapters.RVPlateAdapter
 import com.example.homework1.databinding.FragmentPlateBinding
-import com.example.homework1.utils.Constants
 
 class PlateFragment : Fragment() {
 
-    private var listSize = 0
+    var listSize = 0
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,20 +23,13 @@ class PlateFragment : Fragment() {
             if (resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) 3
             else 4
         val binding = FragmentPlateBinding.inflate(inflater, container, false)
-        savedInstanceState?.let { listSize = it.getInt(Constants.GET_INFO_FROM_INSTANCE) }
         val adapter = RVPlateAdapter(listSize)
         binding.rvPlates.adapter = adapter
         binding.rvPlates.layoutManager = GridLayoutManager(context, spanCount)
         binding.button.setOnClickListener {
             adapter.addPlate()
-            listSize = adapter.itemCount
+            listSize++
         }
         return binding.root
     }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putInt(Constants.GET_INFO_FROM_INSTANCE, listSize)
-    }
-
 }
